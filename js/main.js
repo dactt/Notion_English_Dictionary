c15b98ef2dd9c7dd0a732d9def709afc7eddbecb
LisT <dac.tt290199@gmail.com>
2024-06-12 11:09:55 +0700

    remove redundant mean and example_sentence by using string compare

--- js/main.js
+++ js/main.js
@@ -222,8 +222,7 @@ async function getWordInfo(word) {
         const html = await response.text();
         const parser = new DOMParser();
         const doc = parser.parseFromString(html, 'text/html');
-
-        const pronunciation = doc.querySelector('.pron').textContent;
+        const pronunciation = doc.querySelector('.us.dpron-i').querySelector('.pron').textContent;
         let meaning = '';
         let exampleSentences = '';
         let partOfSpeechAll = [];
@@ -236,15 +235,26 @@ async function getWordInfo(word) {
         }
 
         const defBlocks = doc.querySelectorAll('.def-block');
-
+        var def_array = []
         defBlocks.forEach((defBlock) => {
+            var is_accepted = true
             const def_card = defBlock.querySelector('.def');
             if (def_card) {
                 const def = def_card.textContent.replace(':', '').trim();
                 if (meaning.includes(def)) {
                     return;
                 };
+                def_array.forEach((accepted_def) => {
+                    if (stringSimilarity.compareTwoStrings(accepted_def, def) > 0.7) {
+                        is_accepted = false
+                        return;
+                    }
+                });
+                if (!is_accepted) {
+                    return;
+                }
                 if (meaning.length + def.length < 1900) {
+                    def_array.push(def)
                     meaning += `- ${def}\n`;
                 }
             };
@@ -281,6 +291,64 @@ async function getWordInfo(word) {
     }
 }
 
+! function (t, e) {
+    "object" == typeof exports && "object" == typeof module ? module.exports = e() : "function" == typeof define && define.amd ? define([], e) : "object" == typeof exports ? exports.stringSimilarity = e() : t.stringSimilarity = e()
+}(self, (function () {
+    return t = {
+        138: t => {
+            function e(t, e) {
+                if ((t = t.replace(/\s+/g, "")) === (e = e.replace(/\s+/g, ""))) return 1;
+                if (t.length < 2 || e.length < 2) return 0;
+                let r = new Map;
+                for (let e = 0; e < t.length - 1; e++) {
+                    const n = t.substring(e, e + 2),
+                        o = r.has(n) ? r.get(n) + 1 : 1;
+                    r.set(n, o)
+                }
+                let n = 0;
+                for (let t = 0; t < e.length - 1; t++) {
+                    const o = e.substring(t, t + 2),
+                        s = r.has(o) ? r.get(o) : 0;
+                    s > 0 && (r.set(o, s - 1), n++)
+                }
+                return 2 * n / (t.length + e.length - 2)
+            }
+            t.exports = {
+                compareTwoStrings: e,
+                findBestMatch: function (t, r) {
+                    if (! function (t, e) {
+                        return "string" == typeof t && !!Array.isArray(e) && !!e.length && !e.find((function (t) {
+                            return "string" != typeof t
+                        }))
+                    }(t, r)) throw new Error("Bad arguments: First argument should be a string, second should be an array of strings");
+                    const n = [];
+                    let o = 0;
+                    for (let s = 0; s < r.length; s++) {
+                        const i = r[s],
+                            f = e(t, i);
+                        n.push({
+                            target: i,
+                            rating: f
+                        }), f > n[o].rating && (o = s)
+                    }
+                    return {
+                        ratings: n,
+                        bestMatch: n[o],
+                        bestMatchIndex: o
+                    }
+                }
+            }
+        }
+    }, e = {},
+        function r(n) {
+            if (e[n]) return e[n].exports;
+            var o = e[n] = {
+                exports: {}
+            };
+            return t[n](o, o.exports, r), o.exports
+        }(138);
+    var t, e
+}));
 
 let isFunctionRunning = false;
 let lastExecutionTime = 0;
